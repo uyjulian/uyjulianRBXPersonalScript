44b02f18808f9f3c3c7e49eaa57bb4882eb31c82
uyjulian <uyjulian@gmail.com>
2018-01-03 23:40:01 -0600

    Use specific version of lua-parser

--- prepare-env.sh
+++ prepare-env.sh
@@ -7,9 +7,13 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Install required packages
-apt-get update
-apt-get install luajit luarocks zlib1g-dev
-luarocks install lzlib lua-parser serpent moonscript
+apt-get -y update
+apt-get -y install luajit luarocks zlib1g-dev
+luarocks install lzlib
+# We need this specific version because LPegLabel causes segmentation faults
+luarocks install lua-parser 0.1.1
+luarocks install serpent
+luarocks install moonscript
 
 # Fix moonscript to use luajit
 sed -i 's!/usr/bin/lua5.1!/usr/bin/luajit!' `which moon`
