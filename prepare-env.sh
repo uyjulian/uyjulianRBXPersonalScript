ddeeb2b3df62aacfa1f35f5763e3dff0dbf53467
uyjulian <uyjulian@gmail.com>
2018-03-25 10:35:18 -0500

    Fix segmentation fault occuring on latest lua-parser

--- prepare-env.sh
+++ prepare-env.sh
@@ -10,13 +10,14 @@ fi
 apt-get -y update
 apt-get -y install luajit luarocks zlib1g zlib1g-dev
 luarocks install lua-zlib
-# We need this specific version because LPegLabel causes segmentation faults
-luarocks install lua-parser 0.1.1
+luarocks install lua-parser
 luarocks install serpent
 luarocks install moonscript
 
-# Fix moonscript to use luajit
+# Fix moonscript to use LuaJIT
 sed -i 's!/usr/bin/lua5.1!/usr/bin/luajit!' `which moon`
+# Fix moonscript to use LPegLabel, otherwise segmentation faults occur
+sed -i 's!require("lpeg")!require("lpeglabel")!g' /usr/local/share/lua/5.1/moonscript/errors.lua /usr/local/share/lua/5.1/moonscript/parse.lua /usr/local/share/lua/5.1/moonscript/parse/env.lua /usr/local/share/lua/5.1/moonscript/parse/literals.lua /usr/local/share/lua/5.1/moonscript/parse/util.lua
 
 echo "The environment has been prepared."
 echo "You may now run 'make' to build the program."
